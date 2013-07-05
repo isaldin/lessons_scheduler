9295633293d0c01c33cc3ae4f84ffed3115d1b52
isaldin <saldin.iliya@gmail.com>
2013-07-06 01:43:00 +0600

    lessontype validation, form

--- app/controllers/lesson_types_controller.rb
+++ app/controllers/lesson_types_controller.rb
@@ -1,7 +1,12 @@
 class LessonTypesController < ApplicationController
 
   def index
-    #
+    @lesson_types = LessonType.all
+  end
+
+  def new
+    @lesson_type = LessonType.new
+    @lesson_type
   end
 
 end
--- app/models/lesson_type.rb
+++ app/models/lesson_type.rb
@@ -1,3 +1,9 @@
 class LessonType < ActiveRecord::Base
   attr_accessible :duration, :name, :price_per_student, :pupils_count
+
+  validates :name, :presence => true, :uniqueness => true
+  validates :pupils_count, :presence => true, :numericality => {:greater_than => 0, :less_than => 999}
+  validates :duration, :presence => true, :numericality => {:greater_than => 0, :less_than => 999}
+  validates :price_per_student, :presence => true, :numericality => {:greater_than => 0, :less_than => 999}
+
 end
