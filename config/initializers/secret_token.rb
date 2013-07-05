28802d1d274904993648486e60c0013fffbbae9b
isaldin <saldin.iliya@gmail.com>
2013-07-06 00:48:05 +0600

    initialize

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LessonsScheduler::Application.config.secret_token = '3e34e5da361b85a15ec1ace11d28c41f5d9a1a6c4036465206aff20c69904b26470038de463ef1c36fca8c382fef24cfc1314f71e4a137a5cfac6ea7cf9a17ee'
